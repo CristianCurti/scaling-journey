09050059e9b7a2aaf27a3174a815ecce964fc48e
11202259 <11202259@localhost>
2016-11-17 20:25:39 -0200

    added test folder

---
+++ Exercicio8-monopoly/src/test/java/br/pucrs/fds/exercicio8/monopoly/appTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.pucrs.fds.exercicio8.monopoly;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author 11202259
+ */
+public class appTest {
+    
+    public appTest() {
+    }
+    @Test
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
